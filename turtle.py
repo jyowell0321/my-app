eae4c38f1a3644e7b757b91ca60dfc4d001b1bdf
James <Yowell@Jamess-Air.fios-router.home>
2020-05-17 22:26:32 -0400

    commit

---
+++ turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+wn =turtle.Screen()
+wn.title("Pong by me")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+
+
+
+# Main game loop
+while True:
+    wn.update()
+
