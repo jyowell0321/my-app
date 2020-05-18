5d0aacda80db358f61c40d46e8d12dd59bae0853
James <Yowell@Jamess-Air.fios-router.home>
2020-05-18 04:47:19 -0400

    427 game

--- 4-27amgame.py
+++ 4-27amgame.py
@@ -8,4 +8,24 @@ wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("Space invadersish")
 
+#Draw border
+border_pen= turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300, -300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range (4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+
+delay = input("Press enter to finsh.")
+
+
+
+
+
 
